6ad0f2630dc484558cee0a7a05a9c8b4ec047ee4
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-27 00:26:59 +0000

    HomeBigIdeas.jsx - done

--- src/components/home/components/homeBigIdeas/HomeBigIdeas.tsx
+++ src/components/home/components/homeBigIdeas/HomeBigIdeas.tsx
@@ -1,9 +1,19 @@
 import './homeBigIdeas.css';
+import ArrowButton from '../../../shared/arrowButton/ArrowButton';
 
+/**
+ * Component that renders the "Home Big Ideas" section on the home page.
+ * This section highlights the slogan "Small team, big ideas" and includes a call-to-action button.
+ * 
+ * @returns {JSX.Element} - HomeBigIdeas component. 
+ */
 const HomeBigIdeas:React.FC = () =>{
     return(
         <section id="homeBigIdeas">
-            homeBigIdeas
+            <div>
+                <h3>Small team, big ideas</h3>
+                <ArrowButton text={'About Us'}/>
+            </div>
         </section>
     )
 }
