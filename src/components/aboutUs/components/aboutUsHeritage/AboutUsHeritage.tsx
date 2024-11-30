b3f41a12fda0d8676b681783e4c1a391c5dcaf26
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-30 14:36:09 +0000

    AboutUsHeritage.jsx done

--- src/components/aboutUs/components/aboutUsHeritage/AboutUsHeritage.tsx
+++ src/components/aboutUs/components/aboutUsHeritage/AboutUsHeritage.tsx
@@ -1,9 +1,31 @@
 import './aboutUsHeritage.css';
 
-const AboutUsHeritage:React.FC = () =>{
-    return(
+/**
+ * A component included in the "About Us" section, containing the main text
+ * about the company's heritage and values.
+ * 
+ * @returns {JSX.Element} - The AboutUsHeritage component.
+ */
+const AboutUsHeritage: React.FC = () => {
+    return (
         <section id="aboutUsHeritage">
-            aboutUsHeritage
+            <div></div>
+            <h3>Our Heritage</h3>
+            <div className='defaultText'>
+                <p>
+                    Founded in 2007, we started as a trio of architects. Our complimentary skills
+                    and relentless attention to detail turned Arch into one of the most sought
+                    after boutique firms in the country.
+                </p>
+                <p>
+                    Speciliazing in Urban Design allowed us to focus on creating exceptional
+                    structures that live in harmony with their surroundings. We consider every
+                    detail from every surrounding element to inform our designs.
+                </p>
+                <p>
+                    Our small team of world-class professionals provides input on every project.
+                </p>
+            </div>
         </section>
     )
 }
