2285142b419722967e9c66b939a2e7822af9d920
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-01 00:43:42 +0000

    ContactDetails.tsx - done

--- src/components/contact/components/contactDetails/ContactDetails.tsx
+++ src/components/contact/components/contactDetails/ContactDetails.tsx
@@ -1,9 +1,28 @@
 import './contactDetails.css';
+import ViewOnMapBt from './viewOnMapBt/viewOnMapBt';
 
-const ContactDetails:React.FC = () =>{
-    return(
+const ContactDetails: React.FC = () => {
+    return (
         <section id="contactDetails">
-            contactDetails
+            <h3>Contact Details</h3>
+            <div>
+                <h4>Main office</h4>
+                <div className='contactDetailsInfo'>
+                    <p className='defaultText'>Mail : archone@mail.com</p>
+                    <p className='defaultText'>Address : 1892  Chenoweth Drive TN</p>
+                    <p className='defaultText'>Phone : 123-456-3451</p>
+                </div>
+                <ViewOnMapBt />
+            </div>
+            <div>
+                <h4>Office II</h4>
+                <div className='contactDetailsInfo'>
+                    <p className='defaultText'>Mail : archtwo@mail.com</p>
+                    <p className='defaultText'>Address : 3399  Wines Lane TX</p>
+                    <p className='defaultText'>Phone : 832-123-4321</p>
+                </div>
+                <ViewOnMapBt />
+            </div>
         </section>
     )
 }
---
+++ src/components/contact/components/contactDetails/viewOnMapBt/ViewOnMapBt.tsx
@@ -0,0 +1,8 @@
+import './viewOnMapBt.css';
+
+const ViewOnMapBt: React.FC = () => {
+    return (
+        <button className='viewOnMapBt'>View on Map</button>
+    )
+}
+export default ViewOnMapBt;
