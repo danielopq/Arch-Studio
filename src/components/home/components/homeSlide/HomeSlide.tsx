e244b5affd3040665ea631f993f52e34310fd971
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-01 15:11:53 +0000

    SlideProject.tsx - created

--- src/components/home/components/homeSlide/HomeSlide.tsx
+++ src/components/home/components/homeSlide/HomeSlide.tsx
@@ -1,16 +1,53 @@
 import './homeSlide.css';
 
 import SlideButton from './slideButton/SlideButton';
+import SlideProject from './slideProject/SlideProject';
 
-const HomeSlide:React.FC = () =>{
-    return(
+const HomeSlide: React.FC = () => {
+
+    return (
         <section id="homeSlide">
-            <div></div>
+            <SlideProject
+                zIndex='1'
+                opacity='0'
+                title='Project Paramour'
+                mainText='Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.'
+                desktopImg='./img/home/paramour-desktop.jpg'
+                tabletImg='./img/home/paramour-desktop.jpg'
+                mobileImg='./img/home/paramour-desktop.jpg'
+            />
+            <SlideProject
+                zIndex='2'
+                opacity='0'
+                title='Seraph Station'
+                mainText='The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.'
+                desktopImg='./img/home/seraph-desktop.jpg'
+                tabletImg='./img/home/seraph-desktop.jpg'
+                mobileImg='./img/home/seraph-desktop.jpg'
+            />
+            <SlideProject
+                zIndex='3'
+                opacity='0'
+                title='Federal II Tower'
+                mainText='A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches. '
+                desktopImg='./img/home/federal-desktop.jpg'
+                tabletImg='./img/home/federal-desktop.jpg'
+                mobileImg='./img/home/federal-desktop.jpg'
+            />
+            <SlideProject
+                zIndex='4'
+                opacity='1                                                                                  '
+                title='Trinity Bank Tower'
+                mainText='Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.'
+                desktopImg='./img/home/trinity-desktop.jpg'
+                tabletImg='./img/home/trinity-desktop.jpg'
+                mobileImg='./img/home/trinity-desktop.jpg'
+            />
             <nav id="homeSlideNavBar">
-                <SlideButton text='01' disabled={true}/>
-                <SlideButton text='02' disabled={false}/>
-                <SlideButton text='03' disabled={false}/>
-                <SlideButton text='04' disabled={false}/>
+                <SlideButton text='01' disabled={true} />
+                <SlideButton text='02' disabled={false} />
+                <SlideButton text='03' disabled={false} />
+                <SlideButton text='04' disabled={false} />
             </nav>
         </section>
     )
---
+++ src/components/home/components/homeSlide/slideProject/SlideProject.tsx
@@ -0,0 +1,36 @@
+import './slideProject.css';
+import ArrowButton from '../../../../shared/arrowButton/ArrowButton';
+import { useEffect, useState } from 'react';
+
+interface SlideProjectProps{
+    zIndex:string;
+    opacity:string;
+    title:string;
+    mainText:string;
+    desktopImg: string;
+    tabletImg: string;
+    mobileImg: string;
+}
+
+const SlideProject: React.FC<SlideProjectProps> = ({zIndex,opacity,title,mainText,desktopImg,tabletImg,mobileImg}) => {
+    const [backgroundImage, setBackgroundImage] = useState<string>('');
+
+    useEffect(() => {
+        setBackgroundImage(isDesktop ? desktopImg : isTablet ? tabletImg : mobileImg);
+    }, [])
+
+    const isDesktop: boolean = window.innerWidth > 1440;
+    const isTablet: boolean = window.innerWidth >= 768 && window.innerWidth < 1440;
+    return (
+        <div className='slideProject' style={{ backgroundImage: `url(${backgroundImage})`,opacity:opacity,zIndex:zIndex }}>
+            <div className='slideProjectBg'>
+                <div>
+                    <h2>{title}</h2>
+                    <p className='defaultText'>{mainText}</p>
+                    <ArrowButton text='See Our Portfolio' />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default SlideProject;
