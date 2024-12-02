dd6744e2f5a2461f43368bfd7d83fecaae919b92
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-02 23:35:44 +0000

    SenButton.tsx - created, ContactForm.tsx - first steps

--- src/components/contact/components/contactForm/ContactForm.tsx
+++ src/components/contact/components/contactForm/ContactForm.tsx
@@ -1,9 +1,27 @@
 import './contactForm.css';
-
-const ContactForm:React.FC = () =>{
-    return(
+import SendButton from './sendButton/SendButton';
+const ContactForm: React.FC = () => {
+    return (
         <section id="contactForm">
-            contactForm
+            <h3>Connect with us</h3>
+            <div id="contactForm-content">
+                <form>
+                    <div>
+                        <input type='text' placeholder='Name'></input>
+                        <p className='errorMessage'>Can’t be empty</p>
+                    </div>
+                    <div>
+                        <input type='email' placeholder='Email'></input>
+                        <p className='errorMessage'>Can’t be empty</p>
+                    </div>
+                    <div>
+                        <input type='text' placeholder='Message'></input>
+                        <p className='errorMessage'>Can’t be empty</p>
+                        <SendButton/>
+                    </div>
+
+                </form>
+            </div>
         </section>
     )
 }
---
+++ src/components/contact/components/contactForm/sendButton/SendButton.tsx
@@ -0,0 +1,8 @@
+import './sendButton.css';
+
+const SendButton: React.FC = () => {
+    return (
+        <button className='sendButton'></button>
+    )
+}
+export default SendButton;
