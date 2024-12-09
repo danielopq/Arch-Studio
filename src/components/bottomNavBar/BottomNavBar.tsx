5c18544a3fd527a139c48db2b0f2b28b35fedae9
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-09 15:11:26 +0000

    BottomNavBar.tsx - comments added

--- src/components/bottomNavBar/BottomNavBar.tsx
+++ src/components/bottomNavBar/BottomNavBar.tsx
@@ -2,6 +2,14 @@ import './bottomNavBar.css';
 import NavBar from '../shared/navbar/NavBar';
 import ArrowButton from '../shared/arrowButton/ArrowButton';
 
+/**
+ * Renders the bottom navigation bar of the website.
+ * 
+ * This component provides a compact navigation area located at the bottom of the page.
+ * It includes the main navigation menu and a button for viewing the portfolio.
+ * 
+ * @returns {JSX.Element} The BottomNavBar component.
+ */
 const BottomNavBar: React.FC = () => {
     return (
         <nav id="bottomNavBar">
