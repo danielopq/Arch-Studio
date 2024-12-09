7c44a33d581db07593b04cdebdc9a992dffeb1af
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-09 15:17:00 +0000

    Contact.tsx - comments added

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,9 +4,18 @@ import { ContactDetails, ContactMap, ContactForm } from './components';
 import { useEffect } from 'react';
 
 interface ContactProps {
-    setCurrentSection: (section: 'home' | 'portfolio' | 'about us' | 'contact') => void;
+    setCurrentSection: (section: 'home' | 'portfolio' | 'about us' | 'contact') => void; // Function to set the current section of the website
 };
 
+/**
+ * The "Contact" section of the website.
+ * 
+ * This section provides details for contacting the company, including office addresses, a map of office locations, and a contact form for inquiries.
+ * 
+ * @param {ContactProps} props - The props for the Contact component.
+ * @param {ContactProps} props.setCurrentSection - Function to set the current section of the website.
+ * @returns {JSX.Element} The rendered Contact component.
+ */
 const Contact: React.FC<ContactProps> = ({ setCurrentSection }) => {
     const headerText: string = 'We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!';
     useEffect(() => {
