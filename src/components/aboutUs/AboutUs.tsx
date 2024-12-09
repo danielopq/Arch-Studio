f94ecd8c4f69a2bd9b807800dc32eb07509da59f
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-09 15:01:07 +0000

    AboutUs.tsx - comments added

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -4,9 +4,17 @@ import { AboutUsHeritage, AboutUsLeaders } from './components';
 import SectionHeader from '../shared/sectionHeader/SectionHeader';
 
 interface AboutUsProps {
-    setCurrentSection: (section: 'home' | 'portfolio' | 'about us' | 'contact') => void;
+    setCurrentSection: (section: 'home' | 'portfolio' | 'about us' | 'contact') => void;  // Function to set the current section of the website
 };
 
+/**
+ * Component for rendering the "About Us" section of the website.
+ * This section provides an overview of the team's expertise and values.
+ * 
+ * @param {AboutUsProps} props - The props for the AboutUs component.
+ * @param {function} props.setCurrentSection - Function to update the currently active section of the site.
+ * @returns {JSX.Element} The rendered About Us component.
+ */
 const AboutUs: React.FC<AboutUsProps> = ({ setCurrentSection }) => {
 
     const headerText: string = 'Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.'
