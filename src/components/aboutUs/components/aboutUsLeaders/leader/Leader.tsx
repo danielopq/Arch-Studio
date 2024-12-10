fc7b236c194f45d14b9001efd5d42f6c4dcda150
danielopq <danielopq@gmail.com>
2024-12-10 16:55:45 +0000

    Leader.tsx - comments added

--- src/components/aboutUs/components/aboutUsLeaders/leader/Leader.tsx
+++ src/components/aboutUs/components/aboutUsLeaders/leader/Leader.tsx
@@ -7,6 +7,17 @@ interface LeaderProps{
     gridArea:string;
 }
 
+/**
+ * Leader component displaying information about a company leader.
+ * 
+ * @param {LeaderProps} param0 - The properties for the leader component.
+ * @param {string} param0.name - Name of the leader.
+ * @param {string} param0.position - Position of the leader within the company.
+ * @param {string} param0.backgroundImage - Path to the leader's background image (picture).
+ * @param {string} param0.gridArea - CSS grid area name for positioning.
+ * 
+ * @returns {JSX.Element} The Leader component displaying the leader's name, position, and background image.
+ */
 const Leader: React.FC<LeaderProps> = ({name, position,backgroundImage,gridArea}) => {
     return (
         <div className='leader' style={{ backgroundImage: `url(${backgroundImage})`,gridArea: gridArea}}>
