2b08a9b5dab808b5c1273eb5df5bd95054b2448f
danielopq <danielopq@gmail.com>
2024-12-10 14:40:38 +0000

    ContactForm.tsx - errors controlled by useState

--- src/components/contact/components/contactForm/ContactForm.tsx
+++ src/components/contact/components/contactForm/ContactForm.tsx
@@ -1,15 +1,26 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './contactForm.css';
 import SendButton from './sendButton/SendButton';
 
+interface Errors{
+    nameError:string;
+    emailError:string;
+    messageError:string;
+}
+
 const ContactForm: React.FC = () => {
 
+    const [errors,setErrors] = useState<Errors>({
+        nameError:'',
+        emailError:'',
+        messageError:'',
+    })
+
+    const {nameError,emailError,messageError} = errors;
+
     const refName = useRef<HTMLInputElement>(null);
-    const refNameError = useRef<HTMLParagraphElement>(null);
     const refEmail = useRef<HTMLInputElement>(null);
-    const refEmailError = useRef<HTMLParagraphElement>(null);
     const refMessage = useRef<HTMLTextAreaElement>(null);
-    const refMessageError = useRef<HTMLParagraphElement>(null);
 
     const isEmpty = (text:string):boolean => {
         return (text.trim() === '');
@@ -22,16 +33,26 @@ const ContactForm: React.FC = () => {
 
     const submitForm = (event: React.FormEvent): void => {
         event.preventDefault();
-        if (refName.current && refNameError.current) {
-            isEmpty(refName.current.value) ? refNameError.current.innerText = 'Can’t be empty' : refNameError.current.innerText = '';
+
+        let newError:Errors = {
+            nameError:'',
+            emailError:'',
+            messageError:'',
         }
-        if (refEmail.current && refEmailError.current) {
-            isEmpty(refEmail.current.value) ? refEmailError.current.innerText = 'Can’t be empty' 
-            : isValidEmail(refEmail.current.value) ? refEmailError.current.innerText = '' : refEmailError.current.innerText = 'Invalid format';
+
+        if (refName.current) {
+            isEmpty(refName.current.value) ? newError.nameError = 'Can’t be empty' : newError.nameError = '';
         }
-        if (refMessage.current && refMessageError.current) {
-            isEmpty(refMessage.current.value) ? refMessageError.current.innerText = 'Can’t be empty' : refMessageError.current.innerText = '';
+        if (refEmail.current) {
+            isEmpty(refEmail.current.value) ? newError.emailError = 'Can’t be empty' 
+            : isValidEmail(refEmail.current.value) ? newError.emailError = '' : newError.emailError = 'Invalid format';
         }
+        if (refMessage.current) {
+            isEmpty(refMessage.current.value) ? newError.messageError = 'Can’t be empty' : newError.messageError = '';
+        }
+
+        setErrors(newError);
+        (nameError + emailError + messageError) === '' && console.log('no hay errores');
     }
 
     return (
@@ -41,15 +62,15 @@ const ContactForm: React.FC = () => {
                 <form id="contactForm" onSubmit={submitForm}>
                     <div>
                         <input ref={refName} type='text' placeholder='Name'></input>
-                        <p ref={refNameError}></p>
+                        <p>{nameError}</p>
                     </div>
                     <div>
                         <input ref={refEmail} type='text' placeholder='Email'></input>
-                        <p ref={refEmailError}></p>
+                        <p>{emailError}</p>
                     </div>
                     <div>
                         <textarea ref={refMessage} placeholder='Message'></textarea>
-                        <p ref={refMessageError}></p>
+                        <p>{messageError}</p>
                     </div>
                     <SendButton />
                 </form>
