36f994e6cd93f1f601ac9198273f45e827a437a6
danielopq <danielopq@gmail.com>
2024-12-10 13:57:23 +0000

    ContactForm.tsx - validates empty fields

--- src/components/contact/components/contactForm/ContactForm.tsx
+++ src/components/contact/components/contactForm/ContactForm.tsx
@@ -1,24 +1,51 @@
+import { useRef } from 'react';
 import './contactForm.css';
 import SendButton from './sendButton/SendButton';
+
 const ContactForm: React.FC = () => {
+
+    const refName = useRef<HTMLInputElement>(null);
+    const refNameError = useRef<HTMLParagraphElement>(null);
+    const refEmail = useRef<HTMLInputElement>(null);
+    const refEmailError = useRef<HTMLParagraphElement>(null);
+    const refMessage = useRef<HTMLTextAreaElement>(null);
+    const refMessageError = useRef<HTMLParagraphElement>(null);
+
+    const isEmpty = (text:string):boolean => {
+        return (text.trim() === '');
+    }
+
+    const submitForm = (event: React.FormEvent): void => {
+        event.preventDefault();
+        if (refName.current && refNameError.current) {
+            isEmpty(refName.current.value) ? refNameError.current.innerText = 'Can’t be empty' : refNameError.current.innerText = '';
+        }
+        if (refEmail.current && refEmailError.current) {
+            isEmpty(refEmail.current.value) ? refEmailError.current.innerText = 'Can’t be empty' : refEmailError.current.innerText = '';
+        }
+        if (refMessage.current && refMessageError.current) {
+            isEmpty(refMessage.current.value) ? refMessageError.current.innerText = 'Can’t be empty' : refMessageError.current.innerText = '';
+        }
+    }
+
     return (
         <section id="connectWithUs">
             <h3>Connect with us</h3>
             <div id="connectWithUs-content">
-                <form id="contactForm">
+                <form id="contactForm" onSubmit={submitForm}>
                     <div>
-                        <input type='text' placeholder='Name'></input>
-                        <p>Can’t be empty</p>
+                        <input ref={refName} type='text' placeholder='Name'></input>
+                        <p ref={refNameError}></p>
                     </div>
                     <div>
-                        <input type='email' placeholder='Email'></input>
-                        <p>Can’t be empty</p>
+                        <input ref={refEmail} type='email' placeholder='Email'></input>
+                        <p ref={refEmailError}></p>
                     </div>
                     <div>
-                        <textarea placeholder='Message'></textarea>
-                        <p>Can’t be empty</p>
+                        <textarea ref={refMessage} placeholder='Message'></textarea>
+                        <p ref={refMessageError}></p>
                     </div>
-                    <SendButton/>
+                    <SendButton />
                 </form>
             </div>
         </section>
