2cec899efaf91f70d8f4683790eeeb0aeb6ccaf9
danielopq <danielopq@gmail.com>
2024-12-10 17:08:07 +0000

    ViewOnMapBt.tsx - comments added

--- src/components/contact/components/contactDetails/viewOnMapBt/ViewOnMapBt.tsx
+++ src/components/contact/components/contactDetails/viewOnMapBt/ViewOnMapBt.tsx
@@ -1,5 +1,10 @@
 import './viewOnMapBt.css';
 
+/**
+ * A button component that triggers the action to view the location on the map.
+ * 
+ * @returns {JSX.Element} The button element that allows users to view the location on the map.
+ */
 const ViewOnMapBt: React.FC = () => {
     return (
         <button className='viewOnMapBt'>View on Map</button>
