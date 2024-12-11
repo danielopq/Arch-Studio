421104f260dfbe773380745453737fe05e653f35
danielopq <danielopq@gmail.com>
2024-12-11 16:52:55 +0000

    Home.tsx - comments added

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,6 +6,14 @@ interface HomeProps {
     setCurrentSection: (section: 'home' | 'portfolio' | 'about us' | 'contact') => void;
 };
 
+
+/**
+ * Represents the home page of the website.
+ * 
+ * @param {HomeProps} param0 - The props for the Home component.
+ * @param {(section: 'home' | 'portfolio' | 'about us' | 'contact') => void} param0.setCurrentSection -  Function to update the current section in the website's navigation state.
+ * @returns {JSX.Element} The Home component rendering the home page.
+ */
 const Home: React.FC<HomeProps> = ({setCurrentSection}) => {
 
     useEffect(() => {
