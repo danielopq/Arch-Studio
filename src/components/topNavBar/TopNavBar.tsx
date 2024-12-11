58cee62f3c122dfc9a07be471572a7d3e59dcf2d
danielopq <danielopq@gmail.com>
2024-12-11 17:03:33 +0000

    TopNavBar - comments added

--- src/components/topNavBar/TopNavBar.tsx
+++ src/components/topNavBar/TopNavBar.tsx
@@ -5,6 +5,15 @@ interface TopNavBarProps{
     currentSection:'home' | 'portfolio' | 'about us' | 'contact';
 }
 
+/**
+ * Represents the top navigation bar of the website.
+ * 
+ * Contains the main navigation bar located on the top of the website.
+ * 
+ * @param {TopNavBarProps} param0 - The props for the TopNavBar component.
+ * @param {'home' | 'portfolio' | 'about us' | 'contact'} param0.currentSection - The name of the current section.
+ * @returns {JSX.Element} The TopNavBar component displaying the navigation bar.
+ */
 const TopNavBar:React.FC<TopNavBarProps> = ({currentSection})=>{
     return(
         <nav id="topNavBar">
