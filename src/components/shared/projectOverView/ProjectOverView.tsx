86c1f19e053052172e288e1d3286a3df8214552b
danielopq <danielopq@gmail.com>
2024-12-11 17:27:14 +0000

    ProjectOverView.tsx - comments added

--- src/components/shared/projectOverView/ProjectOverView.tsx
+++ src/components/shared/projectOverView/ProjectOverView.tsx
@@ -11,6 +11,21 @@ interface projectOverViewProps {
     gridArea?: string;
 }
 
+/**
+ * Displays an overview of a project, including a responsive image, title, and brief description.
+ * 
+ * This component dynamically adjusts the displayed background image based on the device's screen size.
+ * 
+ * @param {ProjectOverViewProps} param0 - The props for the ProjectOverView component.
+ * @param {string} [param0.number=''] - Optional. A number representing the project order.
+ * @param {string} param0.title - The title of the project.
+ * @param {string} param0.info - A brief description or information about the project.
+ * @param {string} param0.desktopImg - The URL of the desktop version of the project image.
+ * @param {string} param0.tabletImg - The URL of the tablet version of the project image.
+ * @param {string} param0.mobileImg - The URL of the mobile version of the project image.
+ * @param {string} [param0.gridArea=''] - Optional. Specifies the CSS grid area for the component layout.
+ * @returns {JSX.Element} - The ProjectOverView component displaying the project details and responsive background image.
+ */
 const ProjectOverView: React.FC<projectOverViewProps> = ({ number = '', title, info, desktopImg, tabletImg, mobileImg, gridArea = '' }) => {
 
     const [backgroundImage, setBackgroundImage] = useState<string>('');
