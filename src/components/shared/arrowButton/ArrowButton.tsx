bb914e2bac2f3ce50e0eba68774996a795d14833
danielopq <danielopq@gmail.com>
2024-12-11 17:10:00 +0000

    ArrowButton.tsx - comments added

--- src/components/shared/arrowButton/ArrowButton.tsx
+++ src/components/shared/arrowButton/ArrowButton.tsx
@@ -5,6 +5,14 @@ interface ArrowButtonProps{
     handleClick?:()=>void;
 } 
 
+/**
+ * Renders the button most present in the website.
+ * 
+ * @param param0 
+ * @param {string} param0.text - Text displayed on the button.
+ * @param {function} param0.handleClick - Action given to the button.
+ * @returns {JSX} - ArrowButton component.
+ */
 const ArrowButton: React.FC<ArrowButtonProps> = ({text='',handleClick}) => {
     return (
         <button className='arrowButton' onClick={handleClick}>{text}</button>
