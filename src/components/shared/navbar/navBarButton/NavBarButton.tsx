5a626e512ca75ce210f69149c68992a442a96fac
danielopq <danielopq@gmail.com>
2024-12-11 17:20:56 +0000

    NavBarButton.tsx - comments added

--- src/components/shared/navbar/navBarButton/NavBarButton.tsx
+++ src/components/shared/navbar/navBarButton/NavBarButton.tsx
@@ -8,6 +8,17 @@ interface NavBarButtonProps {
     currentSection?: '' | 'home' | 'portfolio' | 'about us' | 'contact';
 }
 
+/**
+ * Represents a button in the navigation bar.
+ * 
+ * This button allows navigation to different sections of the website. It gets disabled when it matches the current section.
+ * 
+ * @param {NavBarButtonProps} param0 - Props for the NavBarButton component.
+ * @param {string} param0.text - The text displayed on the button. 
+ * @param {string} param0.path - The path to navigate to when the button is clicked.
+ * @param {'' | 'home' | 'portfolio' | 'about us' | 'contact'} [param0.currentSection=''] - The name of the current section. Defaults to an empty string if not provided.
+ * @returns {JSX.Element} The NavBarButton component rendering a navigational button.
+ */
 const NavBarButton: React.FC<NavBarButtonProps> = ({ text, path,currentSection=''}) => {
     const [disabled,setDisabled] = useState<boolean>(false);
 
