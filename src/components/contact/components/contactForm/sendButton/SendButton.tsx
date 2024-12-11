f8cda99c50b354086f8aef15f661ff2509c1ebf3
danielopq <danielopq@gmail.com>
2024-12-11 16:48:11 +0000

    SendButton.tsx - comments added

--- src/components/contact/components/contactForm/sendButton/SendButton.tsx
+++ src/components/contact/components/contactForm/sendButton/SendButton.tsx
@@ -1,5 +1,10 @@
 import './sendButton.css';
 
+/**
+ * Represents the "Send" button used in the ContactForm component.
+ * 
+ * @returns {JSX.Element} The SendButton component..
+ */
 const SendButton: React.FC = () => {
     return (
         <button className='sendButton'></button>
