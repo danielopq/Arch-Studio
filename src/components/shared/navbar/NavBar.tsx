88873a56f42f7653fec9849471a182a4377e5f92
danielopq <danielopq@gmail.com>
2024-12-11 17:15:05 +0000

    NavBar.tsx - comments added

--- src/components/shared/navbar/NavBar.tsx
+++ src/components/shared/navbar/NavBar.tsx
@@ -5,6 +5,16 @@ interface NavBarProps{
     currentSection?: '' | 'home' | 'portfolio' | 'about us' | 'contact';
 }
 
+
+/**
+ * Represents the main navigation bar component.
+ * 
+ * This navigation bar is located at the top and bottom of the website and provides links to the main sections of the site.
+ * 
+ * @param {NavBarProps} param0 - Props for the NavBar component.
+ * @param {'' | 'home' | 'portfolio' | 'about us' | 'contact'} [param0.currentSection=''] - The name of the current section. Defaults to an empty string if not provided.
+ * @returns {JSX.Element} The NavBar component displaying navigation buttons.
+ */
 const NavBar: React.FC<NavBarProps> = ({currentSection=''}) => {
     return (
         <div className='navBar'>
