2296b7586210f2187a66b4be1f89850003921a79
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-15 11:59:42 +0000

    main centered

--- src/components/home/components/homeSlide/HomeSlide.tsx
+++ src/components/home/components/homeSlide/HomeSlide.tsx
@@ -76,8 +76,8 @@ const HomeSlide: React.FC = () => {
                 title='Trinity Bank Tower'
                 mainText='Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.'
                 desktopImg='./img/home/trinity-desktop.jpg'
-                tabletImg='./img/home/trinity-desktop.jpg'
-                mobileImg='./img/home/trinity-desktop.jpg'
+                tabletImg='./img/home/trinity-tablet.jpg'
+                mobileImg='./img/home/trinity-mobile.jpg'
             />
             
             <nav id="homeSlideNavBar">
