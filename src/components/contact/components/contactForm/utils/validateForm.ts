21f0b9bb2e1bbdd6e8a6c2304f70b89af48a9f7c
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-15 21:08:55 +0000

    contactForm.tsx - mobile version done

--- src/components/contact/components/contactForm/utils/validateForm.ts
+++ src/components/contact/components/contactForm/utils/validateForm.ts
@@ -1,7 +1,19 @@
+/**
+ * Checks if a string is empty or consists only of whitespace.
+ * 
+ * @param {string} text - The string to validate.
+ * @returns {boolean} - Returns `true` if the string is empty or only contains whitespace; otherwise, `false`.
+ */
 export const isEmpty = (text: string): boolean => {
     return (text.trim() === '');
 }
 
+/**
+ * Validates if a given string follows a valid email format.
+ * 
+ * @param {string} email - The email address to validate.
+ * @returns {boolean} - Returns `true` if the email format is valid; otherwise, `false`.
+ */
 export const isValidEmail = (email: string): boolean => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
